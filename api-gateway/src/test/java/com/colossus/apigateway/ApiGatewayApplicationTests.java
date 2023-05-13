35624075805887024a1f18986e49a323152d6c41
Kirill Kosykh <triplesonicslash@list.ru>
2023-05-13 20:32:41 +0300

    added api-gateway with properties

---
+++ api-gateway/src/test/java/com/colossus/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colossus.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
