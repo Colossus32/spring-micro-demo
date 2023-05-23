6457e4b1359ba466b1fdffed46dd25bfdbfd17be
Kirill Kosykh <triplesonicslash@list.ru>
2023-05-23 05:20:31 +0300

    eureka client ready

--- eureka-client/src/main/java/com/colossus/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/com/colossus/eurekaclient/controller/TestController.java
@@ -1,13 +1,19 @@
 package com.colossus.eurekaclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/main")
 public class TestController {
+
+    @Value("${eureka.instance.instance-id}")
+    private String id;
 
     @GetMapping ("/test")
     public String test(){
-        return "test controller is alive.";
+        return id;
     }
 }
